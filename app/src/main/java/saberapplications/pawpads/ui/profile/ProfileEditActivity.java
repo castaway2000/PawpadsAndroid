534e8dfd4cdba9b1d332747b9078dd08f12922ff
Stanislav Volnjanskij <stas@kultprosvet.net>
2016-05-11 16:11:11 +0300

    Fixed "Edit profile": Avatar image rotation & upload

--- app/src/main/java/saberapplications/pawpads/ui/profile/ProfileEditActivity.java
+++ app/src/main/java/saberapplications/pawpads/ui/profile/ProfileEditActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
@@ -17,12 +17,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.quickblox.content.QBContent;
 import com.quickblox.content.model.QBFile;
 import com.quickblox.core.QBEntityCallback;
 import com.quickblox.core.QBProgressCallback;
 import com.quickblox.users.QBUsers;
 import com.quickblox.users.model.QBUser;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.InputStreamBody;
@@ -53,7 +56,7 @@ public class ProfileEditActivity extends BaseActivity implements View.OnClickLis
     ImageView img;
     EditText proDescr;
     Button saveBtn, getimgbtn;
-    Uri path;
+    Uri avatarImagePath;
     private QBUser currentQbUser;
     private SharedPreferences defaultSharedPreferences;
     private Bitmap bitmap;
@@ -84,7 +87,7 @@ public class ProfileEditActivity extends BaseActivity implements View.OnClickLis
             @Override
             public void onSuccess(QBUser qbUser, Bundle bundle) {
                 currentQbUser = qbUser;
-                if(currentQbUser.getCustomData()!=null) {
+                if (currentQbUser.getCustomData() != null) {
                     proDescr.setText(String.valueOf(currentQbUser.getCustomData()));
                 }
                 if (currentQbUser.getFileId() != null) {
@@ -149,116 +152,10 @@ public class ProfileEditActivity extends BaseActivity implements View.OnClickLis
                         mSwipeRefreshLayout.setRefreshing(true);
                     }
                 });
-                if (avatarFile != null) {
-                    updateAvatar(avatarFile);
+                if (avatarImagePath != null) {
+                    updateAvatar();
                 }
 
-                    currentQbUser.setCustomData(proDescr.getText().toString());
-                    QBUsers.updateUser(currentQbUser, new QBEntityCallback<QBUser>() {
-                        @Override
-                        public void onSuccess(QBUser user, Bundle args) {
-                            mSwipeRefreshLayout.setRefreshing(false);
-                        }
-
-                        @Override
-                        public void onSuccess() {
-
-                        }
-
-                        @Override
-                        public void onError(List<String> list) {
-                            mSwipeRefreshLayout.setRefreshing(false);
-                            Util.onError(list, ProfileEditActivity.this);
-                        }
-                    });
-                Toast.makeText(getApplicationContext(), "profile saved", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_IMAGE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
-                System.out.println("Image Path : " + selectedImagePath);
-                path = selectedImageUri;
-                try {
-                    bitmap = decodeUri(getApplicationContext(), selectedImageUri, 80);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                img.setImageBitmap(bitmap);
-            }
-        }
-    }
-
-    public String getPath(Uri uri) {
-        return uri.getPath();
-    }
-
-
-    public Bitmap decodeUri(Context c, Uri uri, final int requiredSize)
-            throws FileNotFoundException {
-        BitmapFactory.Options o = new BitmapFactory.Options();
-        o.inJustDecodeBounds = true;
-        InputStream inputStream = c.getContentResolver().openInputStream(uri);
-        avatarFile = getAvatarFile(inputStream);
-        int width_tmp = o.outWidth, height_tmp = o.outHeight;
-        int scale = 1;
-
-        while (true) {
-            if (width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize)
-                break;
-            width_tmp /= 2;
-            height_tmp /= 2;
-            scale *= 2;
-        }
-        BitmapFactory.Options o2 = new BitmapFactory.Options();
-        o2.inSampleSize = scale;
-
-        //TODO: image rotation handeling. EXIF does not work.
-//        Bitmap bitmap = BitmapFactory.decodeFile(uri.getPath(),o2);
-//        Bitmap bm = bitmap;
-//
-//            try {
-//                ExifInterface exif = new ExifInterface(uri.getPath());
-//                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
-//                Matrix m = new Matrix();
-//                if((orientation==3)){
-//                    m.postRotate(180);
-//                    m.postScale((float)bm.getWidth(), (float)bm.getHeight());
-////               if(m.preRotate(90)){
-//                    bitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), m, true);
-//                    return  bitmap;
-//                }
-//                else if(orientation==6){
-//                    m.postRotate(90);
-//                    bitmap = Bitmap.createBitmap(bm, 0, 0,bm.getWidth(),bm.getHeight(), m, true);
-//                    return  bitmap;
-//                }
-//                else if(orientation==8){
-//                    m.postRotate(270);
-//                    bitmap = Bitmap.createBitmap(bm, 0, 0,bm.getWidth(),bm.getHeight(), m, true);
-//                    return  bitmap;
-//                }
-//                return bitmap;
-//            }
-//            catch (Exception e) {
-//            }
-//            return null;
-        return BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o2);
-    }
-
-    private void updateAvatar(File file) {
-
-        QBContent.uploadFileTask(file, false, file.getAbsolutePath(), new QBEntityCallback<QBFile>() {
-            @Override
-            public void onSuccess(QBFile qbFile, Bundle params) {
-
-                int uploadedFileID = qbFile.getId();
-                currentQbUser.setFileId(uploadedFileID);
                 currentQbUser.setCustomData(proDescr.getText().toString());
                 QBUsers.updateUser(currentQbUser, new QBEntityCallback<QBUser>() {
                     @Override
@@ -277,48 +174,132 @@ public class ProfileEditActivity extends BaseActivity implements View.OnClickLis
                         Util.onError(list, ProfileEditActivity.this);
                     }
                 });
+                Toast.makeText(getApplicationContext(), "profile saved", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                avatarImagePath = selectedImageUri;
+                //  try {
+                //    bitmap = decodeUri(getApplicationContext(), selectedImageUri, img.getWidth());
+                Picasso.with(this).load(selectedImageUri)
+                        .resize(img.getWidth(), img.getHeight())
+                        .centerCrop()
+                        .into(img);
+            /*
+            } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+              //  img.setImageBitmap(bitmap);
+            }*/
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+        return uri.getPath();
+    }
+
+
+    private void updateAvatar() {
+// Crop bitmap to 500x500, save it file ,and then upload it to server
+        Picasso.with(this).load(avatarImagePath).resize(500, 500).centerCrop().into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                FileOutputStream out = null;
+                try {
+                    final File file = File.createTempFile("avatar",".jpg",getCacheDir());
+                    out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    out.close();
+
+                    mSwipeRefreshLayout.setRefreshing(true);
+                    QBContent.uploadFileTask(file, false, file.getAbsolutePath(), new QBEntityCallback<QBFile>() {
+                        @Override
+                        public void onSuccess(QBFile qbFile, Bundle params) {
+                            file.delete();
+                            int uploadedFileID = qbFile.getId();
+                            currentQbUser.setFileId(uploadedFileID);
+                            currentQbUser.setCustomData(proDescr.getText().toString());
+                            QBUsers.updateUser(currentQbUser, new QBEntityCallback<QBUser>() {
+                                @Override
+                                public void onSuccess(QBUser user, Bundle args) {
+                                    mSwipeRefreshLayout.setRefreshing(false);
+                                }
+
+                                @Override
+                                public void onSuccess() {
+
+                                }
+
+                                @Override
+                                public void onError(List<String> list) {
+                                    mSwipeRefreshLayout.setRefreshing(false);
+                                    Util.onError(list, ProfileEditActivity.this);
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError(List<String> list) {
+                            mSwipeRefreshLayout.setRefreshing(false);
+                            Util.onError(list, ProfileEditActivity.this);
+                        }
+
+                    }, new QBProgressCallback() {
+                        @Override
+                        public void onProgressUpdate(int progress) {
+
+                        }
+                    });
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Crashlytics.logException(e);
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (Exception e2) {
+
+                    }
+                }
+
+
             }
 
             @Override
-            public void onSuccess() {
+            public void onBitmapFailed(Drawable errorDrawable) {
 
             }
 
             @Override
-            public void onError(List<String> list) {
-                mSwipeRefreshLayout.setRefreshing(false);
-                Util.onError(list, ProfileEditActivity.this);
-            }
-
-        }, new QBProgressCallback() {
-            @Override
-            public void onProgressUpdate(int progress) {
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
 
             }
         });
+
+
     }
 
-    private File getAvatarFile(InputStream inputStream) {
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "tempAvatar.jpg");
-        try {
-            OutputStream os = new FileOutputStream(file);
-            Bitmap bmp = BitmapFactory.decodeStream(inputStream);
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.JPEG, 10, bos);
-            InputStream in = new ByteArrayInputStream(bos.toByteArray());
-            ContentBody photo = new InputStreamBody(in, "image/jpeg", "filename");
-            photo.writeTo(os);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return file;
-    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(avatarFile!=null){
+        if (avatarFile != null) {
             avatarFile.delete();
         }
     }
