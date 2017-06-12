3ef8e1f13e6573efed26b834e856cbc79e4761a4
Stanislav Volnjanskij <stas@kultprosvet.net>
2017-06-12 16:53:37 +0300

    Fixed swipe to refresh on home page

--- app/src/main/java/saberapplications/pawpads/ui/home/ChannelsFragment.java
+++ app/src/main/java/saberapplications/pawpads/ui/home/ChannelsFragment.java
@@ -62,10 +62,6 @@ public class ChannelsFragment extends Fragment implements BaseListAdapter.Callba
         binding.swipelayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                if(adapter.isShowInitialLoad()) {
-                    binding.swipelayout.setRefreshing(false);
-                    return;
-                }
                 adapter.clear();
                 currentPage = 0;
                 loadData();
